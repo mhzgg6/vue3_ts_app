8df95bc75657913bb686c96e017fbcc53f4c7af4
mhz <maohz@dream-it.cn>
2023-02-24 17:54:57 +0800

    feat: vue3学习

---
+++ src/view/test/Children.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="test-children"> 
+    <p>{{ title }}</p>
+    <button @click="onChangeTitle">ssssss</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+type Props = {
+  title: string
+}
+
+const date = ref(1111)
+withDefaults(defineProps<Props>(), {
+  title: 'gg'
+})
+
+const emits = defineEmits(['changeTitle'])
+const onChangeTitle = () => {
+  emits('changeTitle', '黄石路')
+}
+
+//  defineExpose  获取子组件的示例和内部属性
+//  在 script-setup 模式下 数据默认return 给 template 使用 不会暴露到组件外 
+//  将方法 变量暴露给父组件使用 父组件才可以通过 ref api 拿到子组件暴露的数据
+defineExpose({
+  date
+})
+</script>
+
+<style lang='scss' scoped>
+</style>
--- src/view/test/index.vue
+++ src/view/test/index.vue
@@ -6,11 +6,22 @@
     <button @click="change">按钮</button>
     <h4>{{ state }}</h4>
     <h4>{{ state1 }}</h4>
+    <h4>count</h4>
+    <span>{{ count }}</span>--
+    <span>{{ relCount }}</span>
+    <h4>-----watch------</h4>
+    <h4>{{ str }}</h4>
+    <h4>{{ age }}</h4>
+    <h4>{{ tom.age }}-{{ tom.name }}</h4>
+
+    <Children ref="childRef" :title="title" @changeTitle="changeTitle"></Children>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, toRef, reactive } from 'vue'
+import { ref, toRef, reactive, computed, watch } from 'vue'
+import Children from './Children.vue'
+
 type Person = {
   name: string,
   age: number,
@@ -40,6 +51,42 @@ const change = () => {
   state1.value ++
   console.log('obj:', obj, 'state:', state);
 }
+
+const count = ref(200)
+const relCount = computed(() => count.value + 1)
+
+//  监听多个 ref
+const str = ref('彼时此刻')
+const age = ref(18)
+setTimeout(() => { 
+  str.value = '哈哈哈哈哈哈' 
+  age.value = 20
+}, 2000)
+
+watch([str, age], (newVal, oldVal) => {
+  console.log('watch-str:', newVal, oldVal);
+  console.log('watch-str:', newVal, oldVal);
+})
+//  监听reactive
+const tom = reactive({
+  name: 'tom',
+  age: 100
+})
+setTimeout(() => {
+  tom.age = 200
+}, 2000)
+watch(() => tom.age,(newVal, oldVal) => {
+  console.log('watch-tom:', newVal, oldVal);
+})
+
+
+const changeTitle = (msg: string) => {
+  console.log(msg, 'msg');
+}
+
+//  通过 ref 获取子组件属性
+const childRef = ref(null)
+console.log(childRef.value?.date);
 
 </script>
 
